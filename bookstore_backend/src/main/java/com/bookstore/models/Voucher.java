1d48bbf11614b763b2c0e45616498b00fd31d743
TrgMSon <truongminhson1810@gmail.com>
2026-07-04 21:15:05 +0700

    chore(CartDetail, Voucher): bỏ import thừa

--- bookstore_backend/src/main/java/com/bookstore/models/Voucher.java
+++ bookstore_backend/src/main/java/com/bookstore/models/Voucher.java
@@ -1,6 +1,5 @@
 package com.bookstore.models;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
@@ -9,6 +8,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
