125c27a3a7f7b68c6845a77b46c9aa6c80c05b69
TrgMSon <truongminhson1810@gmail.com>
2026-07-04 15:29:42 +0700

    tạo model book

---
+++ bookstore_backend/src/main/java/com/bookstore/models/Book.java
@@ -0,0 +1,45 @@
+package com.bookstore.models;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity
+@Data
+@AllArgsConstructor
+public class Book {
+    @Id
+    @Column(name = "book_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int bookId;
+
+    private String name;
+    private String author;
+    private String description;
+    
+    @Column(name = "quantity_in_stock")
+    private int quantityInStock;
+
+    private String publisher;
+    
+    @Column(name = "publish_year")
+    private String publishYear;
+
+    private BigDecimal price;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @Column(name = "is_delete")
+    private boolean isDelete;
+}
