21e338a3f1feb2426a19de5e13968ee8a1c56fca
Trương Minh Sơn <163323332+TrgMSon@users.noreply.github.com>
2026-07-10 06:54:32 +0700

    Feat (cart): update total

--- bookstore_backend/src/main/java/com/bookstore/repository/CartRepo.java
+++ bookstore_backend/src/main/java/com/bookstore/repository/CartRepo.java
@@ -8,4 +8,11 @@ import com.bookstore.models.Cart;
 
 public interface CartRepo extends JpaRepository<Cart, Integer> {
     Optional<Cart> findByUser_UserId(int userId);
+
+    @Modifying
+    @Transactional
+    @Query(value = """
+            UPDATE cart SET total_amount = total_amount + ?1 WHERE cart_id = ?2
+           """, nativeQuery = true)
+    void updateTotalAmount(BigDecimal updateTotal, int cartId);
 }
